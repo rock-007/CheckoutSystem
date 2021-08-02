03dcffeaf6a34296c38ed5acb84e315badd6d8e3
rock-007 <umair.ashraf2019@gmail.com>
2021-08-02 23:44:43 +0100

    ItemD subClass added to stock package

---
+++ src/main/java/stock/ItemD.java
@@ -0,0 +1,19 @@
+package stock;
+
+public class ItemD extends Item{
+
+    private int ageRestriction;
+
+    public ItemD(String name, long price, int ageRestriction) {
+        super(name, price);
+        this.ageRestriction = ageRestriction;
+    }
+
+    public int getAgeRestriction() {
+        return ageRestriction;
+    }
+
+    public void setAgeRestriction(int ageRestriction) {
+        this.ageRestriction = ageRestriction;
+    }
+}
