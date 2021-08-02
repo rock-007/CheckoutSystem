63c8fe40f86ff1967cbd4d69bb964f4e882ab49f
rock-007 <umair.ashraf2019@gmail.com>
2021-08-02 23:09:26 +0100

    ItemC subClass added to stock package

---
+++ src/main/java/stock/ItemC.java
@@ -0,0 +1,18 @@
+package stock;
+
+public class ItemC extends Item{
+    private int portionSize;
+
+    public ItemC(String name, long price, int portionSize) {
+        super(name, price);
+        this.portionSize = portionSize;
+    }
+
+    public int getPortionSize() {
+        return portionSize;
+    }
+
+    public void setPortionSize(int portionSize) {
+        this.portionSize = portionSize;
+    }
+}
