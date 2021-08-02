e28c9adda7ab7c5bf5a5ebb63e4f3fe314960d81
rock-007 <umair.ashraf2019@gmail.com>
2021-08-02 23:32:50 +0100

    Stock Class added to stock package

---
+++ src/main/java/stock/Stock.java
@@ -0,0 +1,38 @@
+package stock;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Stock {
+
+    private String storeLocation;
+    private Map<Item,Integer> items;
+
+    public Stock(String storeLocation) {
+        this.storeLocation = storeLocation;
+        this.items = new HashMap<>();
+    }
+
+    public String getStoreLocation() {
+        return storeLocation;
+    }
+
+    public void setStoreLocation(String storeLocation) {
+        this.storeLocation = storeLocation;
+    }
+
+    public Map<Item, Integer> getItems() {
+        return items;
+    }
+
+    public void setItems( Item item , Integer quantity) {
+        this.items.put(item, getItemsQuantity(item, quantity));
+    }
+
+    private Integer getItemsQuantity(Item item,Integer quantity) {
+
+       return this.items.getOrDefault(item,0)+quantity;
+    }
+
+
+}
