ac882083eb0b0725180c070bb0f6194332fedf11
rock-007 <umair.ashraf2019@gmail.com>
2021-08-02 23:06:43 +0100

    ItemB subClass added to stock package

---
+++ src/main/java/stock/ItemB.java
@@ -0,0 +1,19 @@
+package stock;
+
+public class ItemB extends Item {
+
+    private int volume;
+
+    public ItemB(String name, long price, int volume) {
+        super(name, price);
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+}
